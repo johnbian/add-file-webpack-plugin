3de7ef53514b3c15233f8941cf2f6bb8a1920250
johnbian <byh19920310@126.com>
2021-08-04 13:56:44 +0800

    init project

---
+++ index.js
@@ -0,0 +1,23 @@
+class AddFileWebpackPlugin {
+	constructor(options) {
+		this.options = options;
+	}
+
+	apply(compiler) {
+		const { paths } = this.options;
+		compiler.hooks.compilation.tap('AddFileWebpackPlugin', (compilation) => {
+			compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap('AddFileWebpackPlugin', (htmlPluginData) => {
+				console.log(paths);
+				for (var i = paths.length - 1; i >= 0; i--) {
+					if(paths[i].type === 'js') {
+						htmlPluginData.assets.js.unshift(paths[i].url);
+					} else if(paths[i].type === 'css') {
+						htmlPluginData.assets.js.unshift(paths[i].url);
+					}
+				}
+			})
+		})
+	}
+}
+
+module.exports = AddFileWebpackPlugin;
